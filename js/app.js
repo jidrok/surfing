b093298bee462a1a4ef72d63d2230eebdc15dee2
jidrok <46357812+jidrok@users.noreply.github.com>
2020-08-06 15:37:42 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
-"use strict";
-
-console.log("llll");
-
-function app() {
-  var nav = $(".nav_content");
-  var burger = $("#burger");
-
-  console.log('t');
-
-  burger.on("click", function i() {
-    event.preventDefault();
-    nav.toggleClass('show');
-    console.log('f');
+$(function(){
+  $(function app() {
+    var nav = $(".nav_content");
+    var burger = $("#burger");
+  
+  
+    burger.on("click", function i() {
+      event.preventDefault();
+      nav.toggleClass('show');
+    });
   });
-};
+});
+
